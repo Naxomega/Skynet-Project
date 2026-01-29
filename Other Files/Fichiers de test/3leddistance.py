bbc969933790a76a4ba16dc385341766d12f43ba
Naxomega.sh <124938856+Naxomega@users.noreply.github.com>
2026-01-29 17:19:37 +0100

    Enhance code readability with comments
    Added comments for better understanding of the code functionality.

--- Other Files/Fichiers de test/3leddistance.py
+++ Other Files/Fichiers de test/3leddistance.py
@@ -1,12 +1,12 @@
-from gpiozero import DistanceSensor, LED
+from gpiozero import DistanceSensor, LED # Setting up gpiozero
 from time import sleep
-led1 = LED(14)
-led2 = LED(15)
-led3 = LED(18)
-ultrasonic = DistanceSensor(echo=17, trigger=4, max_distance=1)
+led1 = LED(14) # }
+led2 = LED(15) # } Setting up LEDs pins
+led3 = LED(18) # }
+ultrasonic = DistanceSensor(echo=17, trigger=4, max_distance=1) # Setting up the Ultrasonic Sensor Pin
 while True:
-    if ultrasonic.distance <= 0.3:
-        led3.on()
+    if ultrasonic.distance <= 0.3: # Powering on and off LEDs if an object if far/close from the sensor
+        led3.on()                  # The expected physical setup is by using a green, an orange/yellow and a red LED 
     if ultrasonic.distance <= 0.2:
         led2.on()
     if ultrasonic.distance <= 0.1:
